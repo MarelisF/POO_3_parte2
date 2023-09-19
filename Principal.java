8a38f84c4b5edfc8c56dbc122dd835a2684ef208
MarelisF <143272611+MarelisF@users.noreply.github.com>
2023-09-18 21:56:54 -0500

    Principal.java

---
+++ Principal.java
@@ -0,0 +1,13 @@
+package com.mycompany.figurasgeometricas;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        Principal princi = new Principal();
+        princi.setVisible(true);
+    }
+}
